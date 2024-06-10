663f4228f1b901f2633ea302e9491f3944f46875
angel4114z <angelandresdiaz40@gmail.com>
2024-06-10 16:41:22 -0500

    register funtion ok

--- project/frontend/templates/register.js
+++ project/frontend/templates/register.js
@@ -16,21 +16,26 @@ document.getElementById('register-form').addEventListener('submit', function(eve
     // Aquí iría la lógica para registrar al usuario
     let url_post = URL_BASE + '/register';
 
-    fetch(url_post, {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({
-            id: 0,
-            email: email,
-            name: name,
-            password: password,
-            list_workspaces: []
-        })
-    });
+    try {
+        fetch(url_post, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                id: 0,
+                email: email,
+                name: name,
+                password: password,
+                list_workspaces: []
+            })
+        });
+    
+    alert("Registro exitoso. Redirigiendo a la página de inicio de sesión...");
+    window.location.href = 'index.html'; // Redirige a la página de inicio de sesión
 
-    //
-    //alert("Registro exitoso. Redirigiendo a la página de inicio de sesión...");
-    //window.location.href = 'index.html'; // Redirige a la página de inicio de sesión
+    } catch (error) {
+        console.error('Error:', error);
+        alert("Error al registrar. Por favor, inténtalo de nuevo.");
+    }
 });
