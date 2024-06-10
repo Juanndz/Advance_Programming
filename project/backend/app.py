6b3ec2b9814a93e1af32bbc27daf4c4ebdb470f3
angel4114z <angelandresdiaz40@gmail.com>
2024-06-10 16:35:12 -0500

    added cors to the backend

--- project/backend/app.py
+++ project/backend/app.py
@@ -2,11 +2,22 @@ from core.user import User
 from core.note import Note
 from core.workspace import Workspace
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 import uvicorn
 
 # author: Angel Diaz
 
 app = FastAPI()
+
+origins = ["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 # ---------------- USER ----------------
 
