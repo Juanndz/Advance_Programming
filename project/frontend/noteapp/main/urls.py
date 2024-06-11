eb59ec681e01d225594aee81ce4f23d76c16c99b
angel4114z <angelandresdiaz40@gmail.com>
2024-06-10 23:05:30 -0500

    user login and register ok

--- project/frontend/noteapp/main/urls.py
+++ project/frontend/noteapp/main/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('login/', views.login, name='login'),
     path('register/', views.register, name='register'),
     path('workspaces/', views.workspaces, name='workspaces'),
 ]
